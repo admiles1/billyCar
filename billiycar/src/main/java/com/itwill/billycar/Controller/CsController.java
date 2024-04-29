6783659a003b8197f675a531bd01df3704a93c1e
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-04-29 15:30:47 +0900

    김유신 / 예약폴더정리

--- billiycar/src/main/java/com/itwill/billycar/Controller/CsController.java
+++ billiycar/src/main/java/com/itwill/billycar/Controller/CsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CsController {
 	
 	@GetMapping("cs")
-	public String event() {
+	public String cs() {
 		return "event/eventPage";
 	}
 	
