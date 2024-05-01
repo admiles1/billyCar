155359bfff804a7e4937fc052eb5a98af74d80c9
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-05-02 08:52:14 +0900

    김유신 / 예약 컨트롤러 매핑 수정

--- billiycar/src/main/java/com/itwill/billycar/Controller/ReservController.java
+++ billiycar/src/main/java/com/itwill/billycar/Controller/ReservController.java
@@ -2,12 +2,21 @@ package com.itwill.billycar.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReservController {
 	
+	
+	// 상탑 차량예약 탭으로 링크 이동시
 	@GetMapping("reservation")
-	public String reservation() {
+	public String reservationGet() {
+		return "reservation/reservation";
+	}
+	
+	// 메인페이지, 예약페이지에서 조건들을 검색할 시
+	@PostMapping("reservation")
+	public String reservationPost() {
 		return "reservation/reservation";
 	}
 	
