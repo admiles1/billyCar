a30fd402442a62de8b000b2b0d225239d6bb8ab0
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-05-02 10:51:06 +0900

    Revert "김유신 / 예약 컨트롤러 매핑 수정"
    This reverts commit 155359bfff804a7e4937fc052eb5a98af74d80c9.

--- billiycar/src/main/java/com/itwill/billycar/Controller/ReservController.java
+++ billiycar/src/main/java/com/itwill/billycar/Controller/ReservController.java
@@ -2,21 +2,12 @@ package com.itwill.billycar.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReservController {
 	
-	
-	// 상탑 차량예약 탭으로 링크 이동시
 	@GetMapping("reservation")
-	public String reservationGet() {
-		return "reservation/reservation";
-	}
-	
-	// 메인페이지, 예약페이지에서 조건들을 검색할 시
-	@PostMapping("reservation")
-	public String reservationPost() {
+	public String reservation() {
 		return "reservation/reservation";
 	}
 	
