8d2746834443f0796f043dc2843ee7475ca994e1
이자민 <amugae111@naver.com>
2024-05-13 10:51:58 +0900

    이자민/ 리다이렉트 추가

--- billiycar/src/main/java/com/itwill/billycar/Controller/MemberController.java
+++ billiycar/src/main/java/com/itwill/billycar/Controller/MemberController.java
@@ -118,7 +118,7 @@ public class MemberController {
 	@GetMapping("logout")
 	public String logout(HttpSession session) {
 		session.invalidate();
-		return "index";
+		return "redirect:/index";
 	}
 	
 	@GetMapping("forgot_id")
