e2ef39c4002c68e0a0a125601e78252b74a5283c
이시윤 <lee_si_yun_@naver.com>
2024-05-13 09:00:48 +0900

    이시윤 - 충돌 커밋

--- billiycar/src/main/java/com/itwill/billycar/Controller/PaymentController.java
+++ billiycar/src/main/java/com/itwill/billycar/Controller/PaymentController.java
@@ -21,11 +21,7 @@ public class PaymentController {
 	
 	// reservation_detail에서 예약하기 버튼 눌렀을 때 컨트롤러
 	@GetMapping("payment")
-<<<<<<< HEAD
 	public String reservation(Model model, CarVO car, MemberVO member) {
-=======
-	public String reservation(Model model) {
->>>>>>> branch 'main' of https://github.com/admiles1/billyCar.git
 		
 		
 		return "payment/paymentPage";
