761a37a9a7afc7dfc5fc0654632f9a7774ac47fa
배소은 <0b506h@gmail.com>
2024-05-14 12:11:55 +0900

    배소은 / 차량 소개

--- billiycar/src/main/java/com/itwill/billycar/Controller/GuideController.java
+++ billiycar/src/main/java/com/itwill/billycar/Controller/GuideController.java
@@ -1,10 +1,21 @@
 package com.itwill.billycar.Controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.itwill.billycar.service.GuideService;
+import com.itwill.billycar.vo.CarVO;
 
 @Controller
 public class GuideController {
+
+	@Autowired
+	private GuideService service;
 	
 	@GetMapping("guideCompany")
 	public String guideCompany() {
@@ -22,7 +33,10 @@ public class GuideController {
 	}
 	
 	@GetMapping("guideCar")
-	public String guideCar() {
+	public String guideCar(Model model) {
+		List<Map<String, Object>> carList = service.carList();
+		System.out.println(carList);
+		model.addAttribute("carList", carList);
 		return "guidance/car_introduction";
 	}
 	
